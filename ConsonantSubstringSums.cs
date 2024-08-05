f6f35336a84bb39bf9a73a67a869230681fe46f0
n-sychev <n.sychev@sh.dunice.net>
2024-08-05 13:44:22 +0300

    ConsonantSubstringSums

---
+++ ConsonantSubstringSums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function;
+
+internal class ConsonantSubstringSums
+{
+    public static int FindMaxConsonantSubstring(string input)
+    {
+        int maxSum = 0;
+
+        int currentSum = 0;
+
+
+        foreach (char symbol in input.ToLower())
+        {
+            if ("aeiou".Contains(symbol))
+            {
+                maxSum = Math.Max(maxSum, currentSum);
+                currentSum = 0;
+            }
+            else
+            {
+                currentSum += symbol % 32;
+            }
+        }
+
+        return Math.Max(maxSum, currentSum);
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Function;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ConsonantSubstringSums.FindMaxConsonantSubstring("zodiacs")); // 26
+
+        Console.WriteLine(ConsonantSubstringSums.FindMaxConsonantSubstring("strength")); // 57
+    }
+}
